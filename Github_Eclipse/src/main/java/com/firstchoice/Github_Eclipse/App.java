af7890f4691a94026ac736dea064c72ec37f5bc7
sam402 <66077604+sam402@users.noreply.github.com>
2020-05-29 17:01:06 -0400

    Update App.java

--- Github_Eclipse/src/main/java/com/firstchoice/Github_Eclipse/App.java
+++ Github_Eclipse/src/main/java/com/firstchoice/Github_Eclipse/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Second change from First Choice developer" );
         System.out.println( "Hello World!" + " First change from First Choice" );
     }
 }
